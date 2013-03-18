bb13cbace74262f8ccc84ed0e5571fd513c449c2
Sasha Laundy <sasha.laundy@gmail.com>
2013-03-18 18:00:27 -0400

    Walk path and remove non .js files

--- walk.py
+++ walk.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env/python
 
 import os
+import re
 
-path = "/Users/sasha/code/markovtwitter/tweets"
+path = "tweets"
 
 for (path, dirs, files) in os.walk(path):
-	print files
+
+	print "before: %s" % files
+
+	for eachfile in files: 
+		if not re.search('\S\.js', eachfile): 
+			print "removing %s" % eachfile
+			files.remove(eachfile)
+
+	print "after %s" % files
