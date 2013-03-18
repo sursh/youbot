6893e47e2cf0f368e0c8dc33184187f2745209ea
Sasha Laundy <sasha.laundy@gmail.com>
2013-03-18 18:22:07 -0400

    Pre-process .js files to remove extraneous line

--- walk.py
+++ walk.py
@@ -3,15 +3,34 @@
 import os
 import re
 
+DEBUG = True
 path = "tweets"
 
-for (path, dirs, files) in os.walk(path):
+def createPath(path):
+  if not os.path.isdir(path):
+    os.mkdir(path)
 
-	print "before: %s" % files
+for (_, _, files) in os.walk(path):
 
-	for eachfile in files: 
-		if not re.search('\S\.js', eachfile): 
-			print "removing %s" % eachfile
-			files.remove(eachfile)
+	del _
 
-	print "after %s" % files
+	# index files & ignore non-.js files
+	for filename in files: 
+		if not re.search('\S\.js', filename): 
+			print "Removing %s." % filename
+			files.remove(filename)
+
+	if DEBUG: print files
+
+	# write out the processed .js tweets to the /processedtweets directory
+	createPath('processedtweets')
+
+	for filename in files:
+		w = open('processedtweets/' + filename, 'w')
+		with open(path + '/' + filename, 'r') as f:
+			f.readline() # ignore first line
+			theRest = f.readlines() 
+			w.write(str(theRest)) # write everything else
+
+	w.close()
+	# Write the processed files to /processedtweets
