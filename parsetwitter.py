69e09b9a16893816f5fe89487224e88243349179
Sasha Laundy <sasha.laundy@gmail.com>
2013-03-19 11:05:05 -0400

    Rip out CSV code and add TODO

--- parsetwitter.py
+++ parsetwitter.py
@@ -9,24 +9,9 @@ def readjson(filename):
   print data # LEFT OFF HERE
   json_data.close
 
-def readcsv(filename):
-  tweets = []
-
-  with open(filename, 'r') as f:
-    i=0
-    for line in f:
-      if line != "\n":
-        if i < 6: print line != "\n", "*** LINE: %s" % line
-        l = line.split(',')
-
-        #if i < 6: print "tweet: %s" % (l[0])
-        i += 1
-
-      #tweets.append(l[7])
-
 def main():
   if len(sys.argv) != 2:
-    print 'usage: ./%s filename' % sys.argv[0]
+    print 'usage: $ python %s filename' % sys.argv[0]
     sys.exit(1)
 
   filename = sys.argv[1]
