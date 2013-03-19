eb681e00d0017d51bc523256d6c07aad09e37f57
Sasha Laundy <sasha.laundy@gmail.com>
2013-03-19 18:14:04 -0400

    Remove data files

--- data/js/payload_details.js
+++
@@ -1,5 +0,0 @@
-var payload_details =  {
-  "tweets" : 30530,
-  "created_at" : "Mon Mar 18 18:05:16 +0000 2013",
-  "lang" : "en"
-}
--- data/js/user_details.js
+++
@@ -1,8 +0,0 @@
-var user_details =  {
-  "screen_name" : "johnsheehan",
-  "location" : "San Francisco, CA",
-  "full_name" : "John Sheehan",
-  "bio" : "Co-founder @Runscope. Also @APIJobs, @APIDigest and @TrafficAndWx.\r\nBefore: @IFTTT, @Twilio, @RestSharp.\r\nI like APIs, Python, Redis, AWS, iOS and stuff.",
-  "id" : "11381522",
-  "created_at" : "Thu Dec 20 19:27:27 +0000 2007"
-}
