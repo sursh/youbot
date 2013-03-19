82c13de646e3a87d27e6a6ecf8782b748032c0d6
Sasha Laundy <sasha.laundy@gmail.com>
2013-03-19 19:22:21 -0400

    Convert 2 spaces to 4

--- markovgenerator.py
+++ markovgenerator.py
@@ -9,129 +9,129 @@ TWEETING = False
 
 class Markov(object):
 
-  def read_file(self, filename):
-    ''' Reads in training corpus text file. Cleans, splits, and adds beginning and end signifiers. '''
+    def read_file(self, filename):
+        ''' Reads in training corpus text file. Cleans, splits, and adds beginning and end signifiers. '''
 
-    with open(filename) as f:
-      for line in f:
-        yield ['^', '^'] + line.strip().lower().split() + ['$']
-
-
-  def generate_trigrams(self, tokens):
-    ''' Break headline into trigrams '''
-
-    trigrams = []
-
-    for idx, item in enumerate(tokens[:-2]):
-      trigrams.append((item, tokens[idx+1], tokens[idx+2]))
-    
-    return trigrams
+        with open(filename) as f:
+            for line in f:
+                yield ['^', '^'] + line.strip().lower().split() + ['$']
 
 
-  def generate_matrix(self, filename):
-    ''' Run through the list of trigrams and add them to the occurence matrix.
+    def generate_trigrams(self, tokens):
+        ''' Break headline into trigrams '''
 
-        There are two data structures here: 
+        trigrams = []
 
-        1) bigrams
-           keys: tuple of first two words in each trigram
-           values: list of all the words that come third in the training corpus
-           e.g. {(a, b): (c1, c2, c3), (b, c1): (d1, d2, d3)}
-
-        2) matrix
-           keys: tuple of each trigram
-           values: tuple of # of occurences of the trigram, and a boolean seenBefore? flag
-           e.g. {(a, b, c): (5, True), (b, c, d): (2: True) ... }
-    '''
-
-    self.matrix = {}
-    self.bigrams = defaultdict(list)
-
-    headlines = self.read_file(filename)
-
-    for headline in headlines:
-
-      trigrams = self.generate_trigrams(headline)
-
-      for trigram in trigrams:
-
-        bigram = trigram[:2]
-        current_word = trigram[-1]
-        (old_count, seenBefore) = self.matrix.get(trigram, (0, False))
-
-        if not seenBefore:
-          self.bigrams[bigram].append(current_word)
-
-        self.matrix[trigram] = (1 + old_count, True)
-
-
-  def generate_next_word(self, prev_word, current_word):
-    ''' Based on prev_word and current_word, returns a third word to follow '''
-
-    bigram = (prev_word, current_word)
-
-    words = []
-    counts = []
-
-    for word in self.bigrams[bigram]:
-      trigram = bigram + (word,)
-      (count, _) = self.matrix[trigram]
-      words.append(word)
-      counts.append(count)
-
-    if not counts: return '$' # aka the ending signifier
-
-    # pick ONE of the possibilities, with probability weighted by frequency in training corpus
-    cumcounts = numpy.cumsum(counts)
-    coin = numpy.random.randint(cumcounts[-1])
-    for index, item in enumerate(cumcounts):
-      if item >= coin:
-        return words[index]
-
-
-  def generate_paragraph(self, initial_word=None):
-    ''' Generates a new headline '''
-    
-    if not initial_word:
-        initial_word = self.generate_next_word('^', '^')
+        for idx, item in enumerate(tokens[:-2]):
+            trigrams.append((item, tokens[idx+1], tokens[idx+2]))
         
-    prev_word = '^'
-    current_word = initial_word
-    paragraph = [ initial_word ]
+        return trigrams
 
-    while (current_word != '$'): 
-      paragraph.append(current_word)
-      prev_word, current_word = current_word, self.generate_next_word( prev_word, current_word )
 
-    paragraph = ' '.join(paragraph[1:])  # strip off leading caret
-    return paragraph
+    def generate_matrix(self, filename):
+        ''' Run through the list of trigrams and add them to the occurence matrix.
+
+                There are two data structures here: 
+
+                1) bigrams
+                     keys: tuple of first two words in each trigram
+                     values: list of all the words that come third in the training corpus
+                     e.g. {(a, b): (c1, c2, c3), (b, c1): (d1, d2, d3)}
+
+                2) matrix
+                     keys: tuple of each trigram
+                     values: tuple of # of occurences of the trigram, and a boolean seenBefore? flag
+                     e.g. {(a, b, c): (5, True), (b, c, d): (2: True) ... }
+        '''
+
+        self.matrix = {}
+        self.bigrams = defaultdict(list)
+
+        headlines = self.read_file(filename)
+
+        for headline in headlines:
+
+            trigrams = self.generate_trigrams(headline)
+
+            for trigram in trigrams:
+
+                bigram = trigram[:2]
+                current_word = trigram[-1]
+                (old_count, seenBefore) = self.matrix.get(trigram, (0, False))
+
+                if not seenBefore:
+                    self.bigrams[bigram].append(current_word)
+
+                self.matrix[trigram] = (1 + old_count, True)
+
+
+    def generate_next_word(self, prev_word, current_word):
+        ''' Based on prev_word and current_word, returns a third word to follow '''
+
+        bigram = (prev_word, current_word)
+
+        words = []
+        counts = []
+
+        for word in self.bigrams[bigram]:
+            trigram = bigram + (word,)
+            (count, _) = self.matrix[trigram]
+            words.append(word)
+            counts.append(count)
+
+        if not counts: return '$' # aka the ending signifier
+
+        # pick ONE of the possibilities, with probability weighted by frequency in training corpus
+        cumcounts = numpy.cumsum(counts)
+        coin = numpy.random.randint(cumcounts[-1])
+        for index, item in enumerate(cumcounts):
+            if item >= coin:
+                return words[index]
+
+
+    def generate_paragraph(self, initial_word=None):
+        ''' Generates a new headline '''
+        
+        if not initial_word:
+                initial_word = self.generate_next_word('^', '^')
+                
+        prev_word = '^'
+        current_word = initial_word
+        paragraph = [ initial_word ]
+
+        while (current_word != '$'): 
+            paragraph.append(current_word)
+            prev_word, current_word = current_word, self.generate_next_word( prev_word, current_word )
+
+        paragraph = ' '.join(paragraph[1:])    # strip off leading caret
+        return paragraph
 
 
 def markov_it(filename, number = 1):
-  print "* Generating tweets. This may take a while."
-  m = Markov()
-  m.generate_matrix(filename) # memory-intensive
+    print "* Generating tweets. This may take a while."
+    m = Markov()
+    m.generate_matrix(filename) # memory-intensive
 
-  if not TWEETING:
-    for i in range(number):
+    if not TWEETING:
+        for i in range(number):
 
-      while True:
-        tweet = m.generate_paragraph() 
-        if len(tweet) < 120: # leave room for RTs
-          break
+            while True:
+                tweet = m.generate_paragraph() 
+                if len(tweet) < 120: # leave room for RTs
+                    break
 
-      print tweet
+            print tweet
 
-  # To use, add your credentials to twitterclient.py
-  if TWEETING:
-    
-    while True:
-      tweet = m.generate_paragraph() 
-      if len(tweet) < 120:
-        break
+    # To tweet, add your credentials to twitterclient.py
+    if TWEETING:
+        
+        while True:
+            tweet = m.generate_paragraph() 
+            if len(tweet) < 120:
+                break
 
-    twitterclient.postTweet(tweet)
+        twitterclient.postTweet(tweet)
 
 
 if __name__ == '__main__':
-  main()
+    main()
--- script.py
+++ script.py
@@ -13,78 +13,78 @@ TWEETLIST = "justTheTweets.txt"
 
 
 def process_archive(path):
-  ''' Crawls /data from twitter and returns list of files 
-      containing tweets '''
+    ''' Crawls /data from twitter and returns list of files 
+            containing tweets '''
 
-  if not os.path.isdir(path):
-    print "Path '%s' not found." % path
-    sys.exit(1)
+    if not os.path.isdir(path):
+        print "Path '%s' not found." % path
+        sys.exit(1)
 
-  tweetfiles = []
+    tweetfiles = []
 
-  for (root, _, files) in os.walk(path):
     # index the files & ignore non-.js files
-    for filename in files[:]: 
-      if re.search('\d{4}_\d{2}.js', filename):
-        tweetfiles.append(os.path.join(root, filename))
-  
-  print "* Found %s months of tweets." % len(files)
-  return tweetfiles 
+    for (root, _, files) in os.walk(path):
+        for filename in files[:]: 
+            if re.search('\d{4}_\d{2}.js', filename):
+                tweetfiles.append(os.path.join(root, filename))
+    
+    print "* Found %s months of tweets." % len(files)
+    return tweetfiles 
 
 
 def parse_files(files, path):
-  ''' Writes text of tweets to a single file '''
+    ''' Writes text of tweets to a single file '''
 
-  if os.path.isfile(TWEETLIST):
-    print "* Using existing file %s." % TWEETLIST
-  
-  else:
-    w = open(TWEETLIST, 'w')
-    print "* Writing tweets to %s." % TWEETLIST
+    if os.path.isfile(TWEETLIST):
+        print "* Using existing file %s." % TWEETLIST
+    
+    else:
+        w = open(TWEETLIST, 'w')
+        print "* Writing tweets to %s." % TWEETLIST
 
-    for filename in files:
-      with open(filename, 'r') as f:
-        f.readline() # ignore the first line; it's gibberish
-        w.write(parse_tweets(f))
+        for filename in files:
+            with open(filename, 'r') as f:
+                f.readline() # ignore the first line; it's gibberish
+                w.write(parse_tweets(f))
 
-    w.close()
+        w.close()
 
 
 def clean_tweets(line):
-  ''' Removes mentions, replies, RTs, and links '''
+    ''' Removes mentions, replies, RTs, and links '''
 
-  line = line.split(' ')
+    line = line.split(' ')
 
-  for token in line[:]:
-    if token and (token[0] == '@' or token == 'RT' or re.search(".*https*:", token)):
-        line.remove(token)
+    for token in line[:]:
+        if token and (token[0] == '@' or token == 'RT' or re.search(".*https*:", token)):
+            line.remove(token)
 
-  return ' '.join(line)
+    return ' '.join(line)
 
 
 def parse_tweets(f):
-  ''' Returns text of all tweets in a given file object'''
+    ''' Returns text of all tweets in a given file object'''
 
-  tweets = []
-  json_data = json.load(f)
-  
-  for tweet in json_data: 
-    payload = tweet['text'].encode('ascii', 'ignore')
-    payload = clean_tweets(payload)
-    tweets.append(payload)
-  
-  return "\n".join(tweets)
+    tweets = []
+    json_data = json.load(f)
+    
+    for tweet in json_data: 
+        payload = tweet['text'].encode('ascii', 'ignore')
+        payload = clean_tweets(payload)
+        tweets.append(payload)
+    
+    return "\n".join(tweets)
 
 
 def main():
 
-  if len(sys.argv) != 2:
-    print 'usage: $ python %s <number_of_tweets>' % sys.argv[0]
-    sys.exit(1)
+    if len(sys.argv) != 2:
+        print 'usage: $ python %s <number_of_tweets>' % sys.argv[0]
+        sys.exit(1)
 
-  fileList = process_archive(PATH)
-  parse_files(fileList, PATH)
-  markovgenerator.markov_it(TWEETLIST, int(sys.argv[1]))
+    fileList = process_archive(PATH)
+    parse_files(fileList, PATH)
+    markovgenerator.markov_it(TWEETLIST, int(sys.argv[1]))
 
 if __name__ == '__main__':
-  main()
+    main()
